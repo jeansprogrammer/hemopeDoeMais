50cab7b495bcdb7e67ea60cdf284769e4387217b
jeansprogrammer <jeansilva20042019@gmail.com>
2025-12-09 15:54:25 -0300

    link de fotos

--- assets/js/map_script.js
+++ assets/js/map_script.js
@@ -3,132 +3,132 @@ const cityData = {
     /*"BUIQUE": {
         nome: "Buíque",
         score: 60.44,
-        img: "../../assets/img/cities_flags/BUIQUE.png"
+        img: "../assets/img/cities_flags/BUIQUE.png"
     },*/
     "ITAIBA": {
         nome: "Itaíba",
         score: 55.21,
-        img: "../../assets/img/cities_flags/ITAIBA.png"
+        img: "../assets/img/cities_flags/ITAIBA.png"
     },
     "AGUAS_BELAS": {
         nome: "Águas Belas",
         score: 58.90,
-        img: "../../assets/img/cities_flags/AGUAS_BELAS.png"
+        img: "../assets/img/cities_flags/AGUAS_BELAS.png"
     },
     /*"TUPANATINGA": {
         nome: "Tupanatinga",
         score: 63.70,
-        img: "../../assets/img/cities_flags/TUPANATINGA.png"
+        img: "../assets/img/cities_flags/TUPANATINGA.png"
     },*/
     /*"PEDRA": {
         nome: "Pedra",
         score: 49.87,
-        img: "../../assets/img/cities_flags/PEDRA.png"
+        img: "../assets/img/cities_flags/PEDRA.png"
     },*/
     "BOM_CONSELHO": {
         nome: "Bom Conselho",
         score: 52.30,
-        img: "../../assets/img/cities_flags/BOM_CONSELHO.png"
+        img: "../assets/img/cities_flags/BOM_CONSELHO.png"
     },
     "IATI": {
         nome: "Iati",
         score: 41.22,
-        img: "../../assets/img/cities_flags/IATI.png"
+        img: "../assets/img/cities_flags/IATI.png"
     },
     "GARANHUNS": {
         nome: "Garanhuns",
         score: 65.76,
-        img: "../../assets/img/cities_flags/GARANHUNS.png"
+        img: "../assets/img/cities_flags/GARANHUNS.png"
     },
     "CANHOTINHO": {
         nome: "Canhotinho",
         score: 59.45,
-        img: "../../assets/img/cities_flags/CANHOTINHO.png"
+        img: "../assets/img/cities_flags/CANHOTINHO.png"
     },
     "CAETES": {
         nome: "Caetés",
         score: 51.90,
-        img: "../../assets/img/cities_flags/CAETES.png"
+        img: "../assets/img/cities_flags/CAETES.png"
     },
     "CORRENTES": {
         nome: "Correntes",
         score: 47.88,
-        img: "../../assets/img/cities_flags/CORRENTES.png"
+        img: "../assets/img/cities_flags/CORRENTES.png"
     },
     /*"VENTUROSA": {
         nome: "Venturosa",
         score: 54.12,
-        img: "../../assets/img/cities_flags/VENTUROSA.png"
+        img: "../assets/img/cities_flags/VENTUROSA.png"
     },*/
     "CAPOEIRAS": {
         nome: "Capoeiras",
         score: 82.33,
-        img: "../../assets/img/cities_flags/CAPOEIRAS.png"
+        img: "../assets/img/cities_flags/CAPOEIRAS.png"
     },
     "SAO_JOAO": {
         nome: "São João",
         score: 81.30,
-        img: "../../assets/img/cities_flags/SAO_JOAO.png"
+        img: "../assets/img/cities_flags/SAO_JOAO.png"
     },
     "PARANATAMA": {
         nome: "Paranatama",
         score: 54.90,
-        img: "../../assets/img/cities_flags/PARANATAMA.png"
+        img: "../assets/img/cities_flags/PARANATAMA.png"
     },
     "LAGOA_DO_OURO": {
         nome: "Lagoa do Ouro",
         score: 55.80,
-        img: "../../assets/img/cities_flags/LAGOA_DO_OURO.png"
+        img: "../assets/img/cities_flags/LAGOA_DO_OURO.png"
     },
     /*"JUREMA": {
         nome: "Jurema",
         score: 48.33,
-        img: "../../assets/img/cities_flags/JUREMA.png"
+        img: "../assets/img/cities_flags/JUREMA.png"
     },*/
     "LAJEDO": {
         nome: "Lajedo",
         score: 58.12,
-        img: "../../assets/img/cities_flags/LAJEDO.png"
+        img: "../assets/img/cities_flags/LAJEDO.png"
     },
     "BREJAO": {
         nome: "Brejão",
         score: 49.22,
-        img: "../../assets/img/cities_flags/BREJAO.png"
+        img: "../assets/img/cities_flags/BREJAO.png"
     },
     "TEREZINHA": {
         nome: "Terezinha",
         score: 76.44,
-        img: "../../assets/img/cities_flags/TEREZINHA.png"
+        img: "../assets/img/cities_flags/TEREZINHA.png"
     },
     "PALMERINA": {
         nome: "Palmerina",
         score: 53.20,
-        img: "../../assets/img/cities_flags/PALMERINA.png"
+        img: "../assets/img/cities_flags/PALMERINA.png"
     },
     "JUPI": {
         nome: "Jupi",
         score: 50.40,
-        img: "../../assets/img/cities_flags/JUPI.png"
+        img: "../assets/img/cities_flags/JUPI.png"
     },
     "ANGELIM": {
         nome: "Angelim",
         score: 42.99,
-        img: "../../assets/img/cities_flags/ANGELIM.png"
+        img: "../assets/img/cities_flags/ANGELIM.png"
     },
     "CALCADO": {
         nome: "Calçado",
         score: 57.10,
-        img: "../../assets/img/cities_flags/CALCADO.png"
+        img: "../assets/img/cities_flags/CALCADO.png"
     },
     "JUCATI": {
         nome: "Jucati",
         score: 77.21,
-        img: "../../assets/img/cities_flags/JUCATI.png"
+        img: "../assets/img/cities_flags/JUCATI.png"
     },
     "SALOA": {
         nome: "Saloá",
         score: 70.21,
-        img: "../../assets/img/cities_flags/SALOA.png"
+        img: "../assets/img/cities_flags/SALOA.png"
     }
 
 };
@@ -155,7 +155,7 @@ Object.keys(cityData).forEach(id => {
         popupScore.textContent = "Pontuação: " + cityData[id].score;
 
         // bandeira real
-        popupFlag.src = cityData[id].img;
+        popupFlag.src = "../" + cityData[id].img;
 
 
         popup.classList.remove("hidden");
